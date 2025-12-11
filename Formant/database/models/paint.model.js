2ce5546bf49b31bcaf4557a553a1f6099368a253
mrDNA219 <nharbourt@gmail.com>
2025-12-11 12:39:13 -0500

    added basic schema for single paint

---
+++ Formant/database/models/paint.model.js
@@ -0,0 +1,32 @@
+import { Schema } from "mongoose";
+
+//FORMAT FOR SINGLE PAINT
+/*
+    <input className='paint-swatch-input' name={color} type='checkbox'></input>
+    <p className='paint-swatch-name'>{color[0]}</p>
+    <label className='paint-swatch-label' htmlFor={color}>{color[1]}</label>
+    <div className='paint-swatch' style={{"backgroundColor" : `${color[2]}`}}></div>
+
+*/
+
+const paintSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    label: {
+        type: String
+    },
+    hexCode: {
+        String
+    }
+});
+
+
+const Paint = mongoose.model('Paints', paintSchema);
+
+export default Paint;
+
+
+
+
+
